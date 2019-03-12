b31ac4437112955efbd4b2a70cfceb089ca1e35c
GVM-LTP-161\patrick.n.manurung <patrick.n.manurung@gvmnetworks.com>
2019-03-13 00:04:24 +0700

    login

--- src/main/java/com/r2r/road2ring/modules/WebController.java
+++ src/main/java/com/r2r/road2ring/modules/WebController.java
@@ -1,7 +1,9 @@
 package com.r2r.road2ring.modules;
 
 
+import com.r2r.road2ring.modules.consumer.Consumer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,7 +16,9 @@ public class WebController {
 
 
   @GetMapping("/login")
-  public String login(){
+  public String login(Model model){
+    Consumer consumer = new Consumer();
+
     return "admin/page/login";
   }
 }
