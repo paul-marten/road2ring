92a6cc9af824a6a335c5c9c097f4c3673d7dee48
Paul Marten Simanjuntak <paul.m.simanjuntak@gvmnetworks.com>
2019-03-12 18:33:22 +0700

    fixing login security for web

--- src/main/java/com/r2r/road2ring/Road2RingApplication.java
+++ src/main/java/com/r2r/road2ring/Road2RingApplication.java
@@ -1,37 +1,31 @@
 package com.r2r.road2ring;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
+@EnableAsync
 public class Road2RingApplication extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(
-      SpringApplicationBuilder application) {
-    return application.sources(Road2RingApplication.class);
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(Road2RingApplication.class, args);
-  }
-
-  @Bean
-  PasswordEncoder getEncoder() {
-    return new BCryptPasswordEncoder();
   }
 
   @Bean
@@ -45,9 +39,24 @@ public class Road2RingApplication extends SpringBootServletInitializer {
         ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error/pagenotfound");
         ErrorPage error500Page =
             new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/internalservererror");
-
         container.addErrorPages(error401Page, error404Page, error500Page);
       }
     };
+  }
+
+  @Bean
+  @ConfigurationProperties(prefix = "datasource.secondary")
+  public DataSource mysqlDataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean(name = "mysqlJdbc")
+  public JdbcTemplate mysqlJdbcTemplate(DataSource mysqlDs) {
+    return new JdbcTemplate(mysqlDataSource());
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Road2RingApplication.class);
   }
 }
