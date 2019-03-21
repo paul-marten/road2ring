7191c0794fd8c01db32b8c952b61302a4794cec2
GVM-LTP-161\patrick.n.manurung <patrick.n.manurung@gvmnetworks.com>
2019-03-21 15:11:08 +0700

    itinerary facility list datatable

--- src/main/java/com/r2r/road2ring/modules/trip/TripAPIController.java
+++ src/main/java/com/r2r/road2ring/modules/trip/TripAPIController.java
@@ -2,12 +2,14 @@ package com.r2r.road2ring.modules.trip;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.r2r.road2ring.modules.common.ResponseView;
+import com.r2r.road2ring.modules.itinerary.Itinerary;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,26 +41,24 @@ public class TripAPIController {
   public List<Trip> datatable(
       HttpServletRequest request) {
 
-//    return tripService.getDatatableContents(input);
     return tripService.getAllTrip();
   }
 
-  @RequestMapping(value = "/facility/data", method = RequestMethod.GET)
-  @JsonView(ResponseView.DetailedTripFacility.class)
-  public List<Trip> datatableFacility(
+  @RequestMapping(value = "/{tripId}/facility/data", method = RequestMethod.GET)
+  @JsonView(ResponseView.DetailedTrip.class)
+  public List<TripFacility> datatableFacility(@PathVariable("tripId") int id,
       HttpServletRequest request) {
+    System.out.println(request.getRequestURI());
 
-//    return tripService.getDatatableContents(input);
-    return tripService.getAllTrip();
+    return tripService.getTripFacility(id);
   }
 
-  @RequestMapping(value = "/itinerary/data", method = RequestMethod.GET)
-  @JsonView(ResponseView.DetailedTripFacility.class)
-  public List<Trip> datatableItinerary(
+  @RequestMapping(value = "/{tripId}/itinerary/data", method = RequestMethod.GET)
+  @JsonView(ResponseView.DetailedTrip.class)
+  public List<Itinerary> datatableItinerary(@PathVariable("tripId") int id,
       HttpServletRequest request) {
 
-//    return tripService.getDatatableContents(input);
-    return tripService.getAllTrip();
+    return tripService.getTripItinerary(id);
   }
 
 }
