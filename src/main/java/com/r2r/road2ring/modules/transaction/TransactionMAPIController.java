1ef0b2390e66bd5d1d6811eaa9bc58fca3da898c
Adji Ginandjar L <adji.lazuardi@gvmnetworks.com>
2019-05-10 11:35:28 +0700

    cors

--- src/main/java/com/r2r/road2ring/modules/transaction/TransactionMAPIController.java
+++ src/main/java/com/r2r/road2ring/modules/transaction/TransactionMAPIController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = M_API)
+@CrossOrigin
 public class TransactionMAPIController {
 
   TransactionService transactionService;
