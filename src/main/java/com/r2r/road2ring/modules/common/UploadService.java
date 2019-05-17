a6c84d0418297acd37b695fcdcc0d17d152b2752
Paul Marten Simanjuntak <paul.m.simanjuntak@gvmnetworks.com>
2019-05-17 14:40:19 +0700

    change service upload image

--- src/main/java/com/r2r/road2ring/modules/common/UploadService.java
+++ src/main/java/com/r2r/road2ring/modules/common/UploadService.java
@@ -9,11 +9,17 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.Date;
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Path;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.tomcat.util.http.fileupload.FileUploadBase.FileSizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,10 +28,16 @@ public class UploadService {
 
   R2rTools r2rTools;
 
+  @Value("${r2r.upload.path}")
+  String imagePath;
+
   @Autowired
   public void setR2rTools(R2rTools r2rTools){
     this.r2rTools = r2rTools;
   }
+
+  @Autowired
+  private HttpServletRequest request;
 
   public String uploadImagePicture(MultipartFile file, String typeImage)
       throws IOException, FileSizeLimitExceededException {
@@ -39,15 +51,15 @@ public class UploadService {
       throw new FileSizeLimitExceededException("File is too Big", file.getSize(), SIZE_100KB);
     }
     linkUrl = date + r2rTools.generateRandomCode(8) + "." + type;
-    File picture = new File(linkUrl);
-    bytes = file.getBytes();
 
-    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(picture));
-    stream.write(bytes);
-    stream.close();
+    String pathtoUploads =  request.getServletContext().getRealPath("/uploads/");
+
+    String pathImageWebApp = pathtoUploads + linkUrl;
+    File picture = new File(pathImageWebApp);
+    file.transferTo(picture);
 
     this.uploadImagePicture(linkUrl, 600,400);
-    this.deleteBaseImage(linkUrl);
+    this.deleteBaseImage(pathImageWebApp);
 
     return linkUrl;
 
@@ -59,9 +71,11 @@ public class UploadService {
   }
 
   private void uploadImagePicture(String name, Integer imgWitdh, Integer imgHeight) throws IOException {
-    File inputFile = new File(name);
+    String pathtoUploads =  request.getServletContext().getRealPath("/uploads/");
+
+    File inputFile = new File(pathtoUploads + name);
     BufferedImage inputImage = ImageIO.read(inputFile);
-    String outputImagePath = IMAGE_ASSETS +name;
+    String outputImagePath = IMAGE_ASSETS + name;
 
     // creates output image
     BufferedImage outputImage = new BufferedImage(imgWitdh,
