d0a1adb6b61f10950037542ecd7cc995cec4ffc2
GVM-LTP-161\patrick.n.manurung <patrick.n.manurung@gvmnetworks.com>
2019-05-27 09:33:03 +0700

    transaction detail and log

--- src/main/java/com/r2r/road2ring/modules/transaction/TransactionController.java
+++ src/main/java/com/r2r/road2ring/modules/transaction/TransactionController.java
@@ -1,6 +1,10 @@
 package com.r2r.road2ring.modules.transaction;
 
 import com.r2r.road2ring.modules.common.ResponseMessage;
+import com.r2r.road2ring.modules.common.ResponseView.DetailedTransaction;
+import com.r2r.road2ring.modules.transactionlog.TransactionLog;
+import com.r2r.road2ring.modules.transactionlog.TransactionLogService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +18,26 @@ public class TransactionController {
 
   TransactionService transactionService;
 
+  TransactionLogService transactionLogService;
+
+  TransactionDetailService transactionDetailService;
+
   @Autowired
   public void setTransactionService(
       TransactionService transactionService) {
     this.transactionService = transactionService;
+  }
+
+  @Autowired
+  public void setTransactionLogService(
+      TransactionLogService transactionLogService) {
+    this.transactionLogService = transactionLogService;
+  }
+
+  @Autowired
+  public void setTransactionDetailService(
+      TransactionDetailService transactionDetailService) {
+    this.transactionDetailService = transactionDetailService;
   }
 
   @RequestMapping(value = "", method = RequestMethod.GET)
@@ -31,8 +51,14 @@ public class TransactionController {
   public String edit(Model model, @RequestParam int id) {
     ResponseMessage response = new ResponseMessage();
     TransactionConfirmationDetailView transaction = transactionService.getTransactionDetailView(id);
+    List<TransactionLog> log = transactionLogService.getAllLogbyTrxID(id);
+    List<TransactionDetail> detail = transactionDetailService.getAllDetailByTrxId(id);
     response.setObject(transaction);
     model.addAttribute("response", response);
+    model.addAttribute("log", log);
+    model.addAttribute("detail", detail);
     return "admin/forms/transaction";
   }
+
+
 }
