f38a414c4e5d2099caa224bc004a3a1876fd7b40
Paul Marten Simanjuntak <paul.m.simanjuntak@gvmnetworks.com>
2019-06-13 23:43:41 +0700

    add request trip

--- src/main/java/com/r2r/road2ring/modules/trip/RequestTripService.java
+++ src/main/java/com/r2r/road2ring/modules/trip/RequestTripService.java
@@ -1,8 +1,28 @@
 package com.r2r.road2ring.modules.trip;
 
+import com.r2r.road2ring.modules.user.User;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RequestTripService {
+
+  RequestTripRepository requestTripRepository;
+
+  @Autowired
+  public void setRequestTripRepository(RequestTripRepository requestTripRepository){
+    this.requestTripRepository = requestTripRepository;
+  }
+
+  public RequestTrip saveRequestTrip(User user,RequestTrip requestTrip){
+    RequestTrip saved = new RequestTrip();
+    saved.setMaxRider(requestTrip.getMaxRider());
+    saved.setStartDate(new Date(requestTrip.getStartTimestamp()));
+    saved.setTrip(requestTrip.getTrip());
+    saved.setUserEmail(user.getEmail());
+    requestTripRepository.save(saved);
+    return saved;
+  }
 
 }
