048da0e0750227b14af11906c21e5b18652fd00b
GVM-LTP-161\patrick.n.manurung <patrick.n.manurung@gvmnetworks.com>
2019-06-27 21:43:59 +0700

    trip filter status, itinerary delete, tinymce itinerary

--- src/main/resources/static/js/si/tripList.js
+++ src/main/resources/static/js/si/tripList.js
@@ -78,12 +78,16 @@ $(document).ready( function () {
   });
 
   var btnNew = '<a href="/trip/add" class="btn btn-default btn-sm"><span class="fa fa-plus-circle fa-lg"></span> Add New Record</a>';
-  var filterStatus = 'Filter by : <select class="form-control isPublished"><option value="">--- All Status ---</option><option value="0">Unpublish Content</option><option value="1">Published Content</option><option value="3">Scheduled</option></select>';
+  var filterStatus = 'Filter by : <select class="form-control isPublished"><option value="">--- All Status ---</option><option value="UNPUBLISHED">Unpublish Content</option><option value="PUBLISHED">Published Content</option></select>';
   var filterCaptain = '&nbsp;<input class="form-control findCaptain" size="24" type="text" name="findCaptain" placeholder="Find Specific Captain">';
   var filterTitle = '&nbsp;<input class="form-control findTitle" size="47" type="text" name="findTitle" placeholder="Find Specific Title">';
   var filter = filterStatus +  filterTitle + filterCaptain;
   $("div.newRecord").html(btnNew);
   $("div.toolbar").html(filter);
+
+  $('.isPublished').on('change', function() {
+    table.columns(4).search(this.value).draw();
+  });
 
   $('.findTitle').on('keyup', function(event) {
       if ($(this).val().length > 2)
